b93facee7d897efd2a3145a342f0d634ff228df7
AndyOK <andyortegakou@yahoo.com.mx>
2025-03-17 22:57:27 -0600

    feat: update generated lexer with new token rules

--- thelexer.py
+++ thelexer.py
@@ -26,8 +26,9 @@ class Lexer:
                 local_env = {'lexeme': lexeme, 'text': text}
                 action_code = selected_rule['action'].replace('return', 'token =')
                 exec(action_code, {}, local_env)
-                token = local_env.get('token', lexeme)
-                tokens.append(token)
+                token = local_env.get('token', None)
+                if token is not None:
+                   tokens.append(token)
                 pos += longest_match
         return tokens
 
@@ -43,17 +44,7 @@ class Lexer:
         postfix = parser.to_postfix()
         syntax_tree = SyntaxTree(postfix)
         dfa = DFA(syntax_tree)
-        rules.append({'regex': r'''[' ' '\t']#''', 'action': r'''return "skip"''', 'dfa': dfa})
-        from models.regex_parser import RegexParser
-        from models.syntax_tree import SyntaxTree
-        from models.dfa import DFA
-        # Regla: ['\n']#
-        parser = RegexParser(r'''['\n']#''')
-        parser.tokenize()
-        postfix = parser.to_postfix()
-        syntax_tree = SyntaxTree(postfix)
-        dfa = DFA(syntax_tree)
-        rules.append({'regex': r'''['\n']#''', 'action': r'''return "EOL"''', 'dfa': dfa})
+        rules.append({'regex': r'''[' ' '\t']#''', 'action': r'''return None''', 'dfa': dfa})
         from models.regex_parser import RegexParser
         from models.syntax_tree import SyntaxTree
         from models.dfa import DFA
@@ -63,7 +54,7 @@ class Lexer:
         postfix = parser.to_postfix()
         syntax_tree = SyntaxTree(postfix)
         dfa = DFA(syntax_tree)
-        rules.append({'regex': r'''([0-9])+#''', 'action': r'''return int(lexeme)''', 'dfa': dfa})
+        rules.append({'regex': r'''([0-9])+#''', 'action': r'''return ("NUMBER", int(lexeme))''', 'dfa': dfa})
         from models.regex_parser import RegexParser
         from models.syntax_tree import SyntaxTree
         from models.dfa import DFA
@@ -73,7 +64,7 @@ class Lexer:
         postfix = parser.to_postfix()
         syntax_tree = SyntaxTree(postfix)
         dfa = DFA(syntax_tree)
-        rules.append({'regex': r''''+'#''', 'action': r'''return "PLUS"''', 'dfa': dfa})
+        rules.append({'regex': r''''+'#''', 'action': r'''return ("PLUS", "+")''', 'dfa': dfa})
         from models.regex_parser import RegexParser
         from models.syntax_tree import SyntaxTree
         from models.dfa import DFA
@@ -83,7 +74,37 @@ class Lexer:
         postfix = parser.to_postfix()
         syntax_tree = SyntaxTree(postfix)
         dfa = DFA(syntax_tree)
-        rules.append({'regex': r''''-'#''', 'action': r'''return "MINUS"''', 'dfa': dfa})
+        rules.append({'regex': r''''-'#''', 'action': r'''return ("MINUS", "-")''', 'dfa': dfa})
+        from models.regex_parser import RegexParser
+        from models.syntax_tree import SyntaxTree
+        from models.dfa import DFA
+        # Regla: '*'#
+        parser = RegexParser(r''''*'#''')
+        parser.tokenize()
+        postfix = parser.to_postfix()
+        syntax_tree = SyntaxTree(postfix)
+        dfa = DFA(syntax_tree)
+        rules.append({'regex': r''''*'#''', 'action': r'''return ("TIMES", "*")''', 'dfa': dfa})
+        from models.regex_parser import RegexParser
+        from models.syntax_tree import SyntaxTree
+        from models.dfa import DFA
+        # Regla: '('#
+        parser = RegexParser(r''''('#''')
+        parser.tokenize()
+        postfix = parser.to_postfix()
+        syntax_tree = SyntaxTree(postfix)
+        dfa = DFA(syntax_tree)
+        rules.append({'regex': r''''('#''', 'action': r'''return ("LPAREN", "(")''', 'dfa': dfa})
+        from models.regex_parser import RegexParser
+        from models.syntax_tree import SyntaxTree
+        from models.dfa import DFA
+        # Regla: ')'#
+        parser = RegexParser(r'''')'#''')
+        parser.tokenize()
+        postfix = parser.to_postfix()
+        syntax_tree = SyntaxTree(postfix)
+        dfa = DFA(syntax_tree)
+        rules.append({'regex': r'''')'#''', 'action': r'''return ("RPAREN", ")")''', 'dfa': dfa})
         from models.regex_parser import RegexParser
         from models.syntax_tree import SyntaxTree
         from models.dfa import DFA
@@ -93,6 +114,16 @@ class Lexer:
         postfix = parser.to_postfix()
         syntax_tree = SyntaxTree(postfix)
         dfa = DFA(syntax_tree)
-        rules.append({'regex': r'''eof#''', 'action': r'''raise Exception("Fin de buffer")''', 'dfa': dfa})
+        rules.append({'regex': r'''eof#''', 'action': r'''return ("EOF", None)''', 'dfa': dfa})
+        from models.regex_parser import RegexParser
+        from models.syntax_tree import SyntaxTree
+        from models.dfa import DFA
+        # Regla: '\n'#
+        parser = RegexParser(r''''\n'#''')
+        parser.tokenize()
+        postfix = parser.to_postfix()
+        syntax_tree = SyntaxTree(postfix)
+        dfa = DFA(syntax_tree)
+        rules.append({'regex': r''''\n'#''', 'action': r'''return ("EOL", None)''', 'dfa': dfa})
         return rules
 
