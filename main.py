61a8e669a6dde504bd7e5d04d87f1508f4d5431a
AndyOK <andyortegakou@yahoo.com.mx>
2025-03-17 22:56:35 -0600

    fix: ensure correct lexer generation

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # main.py
 
-from controllers.main_controller import run_app, test_full_pipeline
+from controllers.main_controller import run_app, test_full_pipeline, generate_lexer
 
 if __name__ == "__main__":
-    test_full_pipeline("inputs/lexer.yal")
+    # test_full_pipeline("inputs/lexer.yal")
+    generate_lexer()
 
     # run_app()
