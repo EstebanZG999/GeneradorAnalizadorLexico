b2f1c03fc3dff765df81fa4fc5528716c1a69455
Esteban Z <zam22119@uvg.edu.gt>
2025-03-22 00:40:04 -0600

    feat: testing new features

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # main.py
 
-from controllers.main_controller import run_app, test_full_pipeline, generate_lexer
+from controllers.main_controller import run_app, test_full_pipeline, generate_lexer, generate_global_dfa
+from models.mindfa import minimize_dfa, render_mindfa
+
 
 if __name__ == "__main__":
     # test_full_pipeline("inputs/lexer.yal")
-    generate_lexer()
+    global_dfa = generate_global_dfa()
+    min_dfa = minimize_dfa(global_dfa)
+    render_mindfa(min_dfa, "global_dfa_minimized")
+    #generate_lexer()
 
     # run_app()
