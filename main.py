7eda518bf12646374b10434c1eacdfb94455c12c
AndyOK <andyortegakou@yahoo.com.mx>
2025-03-25 00:07:19 -0600

    docs: edited to run test_lexer

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from models.mindfa import minimize_dfa, render_mindfa
 
 if __name__ == "__main__":
     # test_full_pipeline("inputs/lexer.yal")
-    global_dfa = generate_global_dfa()
-    min_dfa = minimize_dfa(global_dfa)
-    render_mindfa(min_dfa, "global_dfa_minimized")
-    #generate_lexer()
+    # global_dfa = generate_global_dfa()
+    # min_dfa = minimize_dfa(global_dfa)
+    # render_mindfa(min_dfa, "global_dfa_minimized")
+    generate_lexer()
 
     # run_app()
