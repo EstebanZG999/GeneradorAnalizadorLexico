ccf1657a0474046103a9a698f6a7174a7ecb9586
EstebanZG999 <zam22119@uvg.edu.gt>
2025-04-27 13:31:32 -0600

    feat(runtime): relocate cli_view into src/runtime/views and adjust imports

---
+++ src/runtime/token_types.py
@@ -0,0 +1,13 @@
+# src/runtime/token_types.py
+
+SKIP  = "skip"
+EOL   = "EOL"
+PLUS  = "PLUS"
+MINUS = "MINUS"
+TIMES = "TIMES"
+LPAREN= "LPAREN"
+RPAREN= "RPAREN"
+EOF   = "EOF"
+WORD  = "WORD"
+LETTER= "LETTER"
+NUMBER= "NUMBER"
